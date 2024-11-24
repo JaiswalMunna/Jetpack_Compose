33dca6681ce305c01f0dfbd57986c1c0024a34db
MunnaJaiswal <codermunna007@gmail.com>
2024-11-24 14:56:43 +0545

    Text concept completed.

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -8,23 +8,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.input.pointer.motionEventSpy
-import  androidx.compose.ui.text.*
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
-import java.time.format.TextStyle
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +33,10 @@ class MainActivity : ComponentActivity() {
                         TextShadow()
                         MultipleStylesInText()
                         CenterText()
+                        LongText()
+                        SimpleOutlinedTextFieldSample()
+                        PasswordTextField()
+                        NoLeadingZeroes()
                     }
                 }
             }
--- app/src/main/java/com/example/jetpackcompose/Text.kt
+++ app/src/main/java/com/example/jetpackcompose/Text.kt
@@ -1,17 +1,26 @@
 package com.example.jetpackcompose
 
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,4 +60,44 @@ fun MultipleStylesInText() {
 @Composable
 fun CenterText() {
     Text("Hello World", textAlign = TextAlign.Center, modifier = Modifier.width(150.dp))
+}
+
+@Composable
+fun LongText() {
+    Text("hello ".repeat(500), maxLines = 5, overflow = TextOverflow.Ellipsis)
+}
+
+@Composable
+fun SimpleOutlinedTextFieldSample() {
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        label = { Text("Enter your name") }
+    )
+}
+
+@Composable
+fun PasswordTextField() {
+    var password by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Enter password") },
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+    )
+}
+
+@Composable
+fun NoLeadingZeroes() {
+    var input by rememberSaveable { mutableStateOf("") }
+    TextField(
+        value = input,
+        onValueChange = { newText ->
+            input = newText.trimStart { it == '0' }
+        }
+    )
 }
