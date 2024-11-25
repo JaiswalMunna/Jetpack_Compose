37ad34a9e16625d0dcffa136b403d84fb82b9ecf
MunnaJaiswal <codermunna007@gmail.com>
2024-11-25 13:51:45 +0545

    card concept

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcompose
 
 import android.os.Bundle
+import android.widget.HorizontalScrollView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,13 +11,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.app_layout.BadgeExample
 import com.example.jetpackcompose.app_layout.BadgeInteractiveExample
+import com.example.jetpackcompose.app_layout.CardMinimalExample
+import com.example.jetpackcompose.app_layout.ElevatedButtonExample
+import com.example.jetpackcompose.app_layout.ElevatedCardExample
+import com.example.jetpackcompose.app_layout.FilledButtonExample
+import com.example.jetpackcompose.app_layout.FilledCardExample
+import com.example.jetpackcompose.app_layout.FilledTonalButtonExample
+import com.example.jetpackcompose.app_layout.OutlinedButtonExample
+import com.example.jetpackcompose.app_layout.OutlinedCardExample
 import com.example.jetpackcompose.app_layout.PartialBottomSheet
+import com.example.jetpackcompose.app_layout.TextButtonExample
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 
@@ -26,16 +37,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeTheme {
-                    Scaffold(modifier = Modifier.fillMaxSize()) { contentPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { contentPadding ->
 
-                        Column(
-                            modifier = Modifier
-                                .padding(contentPadding)
-                                .fillMaxSize()
-                                .padding(16.dp),
-                            verticalArrangement = Arrangement.spacedBy(15.dp)
-                        ) {
-                            //Text Layout
+                    Column(
+                        modifier = Modifier
+                            .padding(contentPadding)
+                            .fillMaxSize()
+                            .padding(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(15.dp)
+                    ) {
+                        //Text Layout
 //                            CustomText()
 //                            TextShadow()
 //                            MultipleStylesInText()
@@ -45,14 +56,37 @@ class MainActivity : ComponentActivity() {
 //                            PasswordTextField()
 //                            NoLeadingZeroes()
 
-                            //App layout
-                            BadgeExample()
-                            BadgeInteractiveExample()
-                            PartialBottomSheet()
-                        }
+                        //App layout
+//                        BadgeExample()
+//                        BadgeInteractiveExample()
+//                        PartialBottomSheet()
                     }
+
+//                    Row (
+//                        modifier = Modifier
+//                            .padding(contentPadding)
+//                            .fillMaxSize()
+//                            .padding(16.dp)
+//                            .horizontalScroll(rememberScrollState()),
+//                        horizontalArrangement = Arrangement.spacedBy(15.dp)
+//                    ){
+//                        FilledButtonExample()
+//                        FilledTonalButtonExample()
+//                        OutlinedButtonExample()
+//                        ElevatedButtonExample()
+//                        TextButtonExample()
+//                    }
+                    Column (modifier = Modifier.padding(15.dp)){
+                        CardMinimalExample()
+                        FilledCardExample()
+                        ElevatedCardExample()
+                        OutlinedCardExample()
+                    }
+
+                }
             }
         }
     }
 }
+
 
--- app/src/main/java/com/example/jetpackcompose/app_layout/buttons.kt
+++ app/src/main/java/com/example/jetpackcompose/app_layout/buttons.kt
@@ -1,2 +1,68 @@
 package com.example.jetpackcompose.app_layout
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FilledButtonExample(
+//    onClick: () -> Unit
+) {
+    Button(onClick = {
+//        onClick()
+    }) {
+        Text("Filled")
+    }
+}
+
+@Composable
+fun FilledTonalButtonExample(
+//    onClick: () -> Unit
+) {
+    FilledTonalButton(
+        onClick = {
+//            onClick()
+        }) {
+        Text("Tonal")
+    }
+}
+
+@Composable
+fun OutlinedButtonExample(
+//    onClick: () -> Unit
+) {
+    OutlinedButton(
+        onClick = {
+//            onClick()
+        }) {
+        Text("Outlined")
+    }
+}
+
+@Composable
+fun ElevatedButtonExample(
+//    onClick: () -> Unit
+) {
+    ElevatedButton(onClick = {
+//        onClick()
+    }) {
+        Text("Elevated")
+    }
+}
+
+@Composable
+fun TextButtonExample(
+//    onClick: () -> Unit
+) {
+    TextButton(
+        onClick = {
+//            onClick()
+        }
+    ) {
+        Text("Text Button")
+    }
+}
